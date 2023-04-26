337d4f95dccf43c046e19584c4980c01c5c3cc8c
jerndal <jerndal.petter@gmail.com>
2023-04-26 15:02:23 +0200

    Tweaked the functionality whe

--- ev3/basicmovement/main.py
+++ ev3/basicmovement/main.py
@@ -21,22 +21,37 @@ from pybricks.robotics import DriveBase
 ev3 = EV3Brick()
 #Initialize the distance sensor
 
-sensor = UltrasonicSensor(Port.S1)
+sensor1 = UltrasonicSensor(Port.S1)
 
 # Initialize the motors.
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
+spinning_motor = Motor(Port.D)
 
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 drive = True
+speed = -100
+
 while drive:
+    # Spin the sensor back and forth
+    print(spinning_motor.angle())
+    spinning_motor.run(speed=speed)
+    if spinning_motor.angle() <= -90:
+         print("Change direction")
+         speed = 40
+    if spinning_motor.angle() >= 90:
+        print("Change direction")
+        speed = -40
     robot.drive(speed=-100, turn_rate=0)
-    if sensor.distance() < 200:
+
+    # Handle object detection
+    if sensor1.distance() < 200:
         #beep the horn
         ev3.speaker.beep()
         #stop and turn left travel for 30cm and then turn right again
         robot.stop()
+        robot.straight(100)
         robot.turn(90)
         robot.straight(-300)
         robot.turn(-90)
