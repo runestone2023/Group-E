3cb593479976209a7aa108032d8d59cb0c71b705
jerndal <jerndal.petter@gmail.com>
2023-04-26 11:48:06 +0200

    basic movement and object detection

--- ev3/basicmovement/main.py
+++ ev3/basicmovement/main.py
@@ -12,16 +12,16 @@ https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#ro
 """
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
+from pybricks.ev3devices import Motor, UltrasonicSensor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
-from pybricks.pupdevices import UltrasonicSensor
 
 
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
 #Initialize the distance sensor
-sensor = UltrasonicSensor(Port.A)
+
+sensor = UltrasonicSensor(Port.S1)
 
 # Initialize the motors.
 left_motor = Motor(Port.B)
@@ -31,16 +31,16 @@ right_motor = Motor(Port.C)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 drive = True
 while drive:
-    robot.drive()
+    robot.drive(speed=-100, turn_rate=0)
     if sensor.distance() < 200:
         #beep the horn
         ev3.speaker.beep()
         #stop and turn left travel for 30cm and then turn right again
         robot.stop()
-        robot.turn(-90)
-        robot.straight(300)
         robot.turn(90)
+        robot.straight(-300)
+        robot.turn(-90)
         continue
     else:
-        continue
+         continue
 
