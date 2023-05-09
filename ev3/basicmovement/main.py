d3f02c6563d9f8883359e99beaf3d433c1b9f2e0
jerndal <jerndal.petter@gmail.com>
2023-05-09 12:16:12 +0200

    Second iteration of robot object detection and avoidance

--- ev3/basicmovement/main.py
+++ ev3/basicmovement/main.py
@@ -15,7 +15,8 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, UltrasonicSensor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
-from server import app
+# from server import app
+import time
 
 
 # Initialize the EV3 Brick.
@@ -33,29 +34,45 @@ spinning_motor = Motor(Port.D)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 drive = True
 speed = -100
+glassbilen = ["G/8","C/8","E/8","C/8","G/8", "G/8", "C/8", "E/8","C/8","G/8","F/8","F/8","D/8","D/8","C/8"]
 
 while drive:
-    # Spin the sensor back and forth
+    # Play low putched tone to indicate no object is detected
+    # ev3.speaker.play_notes(notes=glassbilen)
+    # # Spin the sensor back and forth
+    ev3.speaker.beep(300, -5)
     print(spinning_motor.angle())
     spinning_motor.run(speed=speed)
     if spinning_motor.angle() <= -90:
          print("Change direction")
-         speed = 40
+         speed = 200
     if spinning_motor.angle() >= 90:
         print("Change direction")
-        speed = -40
+        speed = -200
     robot.drive(speed=-100, turn_rate=0)
 
     # Handle object detection
-    if sensor1.distance() < 200:
-        #beep the horn
-        ev3.speaker.beep()
-        #stop and turn left travel for 30cm and then turn right again
+    if sensor1.distance() < 300:
+        # Play high pitched sound to indicate that an object is detected
+        ev3.speaker.beep(500, 500)
+        # Stop and turn left travel for 30cm and then turn right again
+        spinning_motor.stop()
+        spinning_motor.run_target(speed=500, target_angle=0)
         robot.stop()
-        robot.straight(100)
-        robot.turn(90)
-        robot.straight(-300)
-        robot.turn(-90)
+        if spinning_motor.angle() > 0 :
+            robot.straight(150)
+            robot.turn(-120)
+            ev3.speaker.beep(800, 200)
+            robot.straight(-200)
+            robot.turn(120)
+        else:
+            robot.straight(150)
+            robot.turn(120)
+            ev3.speaker.beep(800, 200)
+            time.sleep(0.2)
+            ev3.speaker.beep(800, 200)
+            robot.straight(-200)
+            robot.turn(-120)
         continue
     else:
          continue
